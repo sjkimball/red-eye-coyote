7112e59c44d70fa9bbde4b6d12a9416c1ed572f7
Sam Kimball <samuel.j.kimball@gmail.com>
2019-08-16 01:52:39 -0500

    Query starter data, map ProjectPreview components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,54 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
+import { graphql, useStaticQuery } from 'gatsby';
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import ProjectPreview from "../components/project-preview"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export default () => {
+  const data = useStaticQuery(graphql`
+    {
+      allProjectsJson {
+        edges {
+          node {
+            title
+            slug
+            description
+            image {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }      
+    }
+  `);
 
-export default IndexPage
+    const projects = data.allProjectsJson.edges;
+
+  return(
+    <Layout>
+      <SEO title="Home" />
+      {projects.map(({ node: project }) => {
+        const title = project.title;
+        const description = project.description;
+        const slug = project.slug;
+        const imageData = project.image.childImageSharp.fluid;
+
+        return (
+          <ProjectPreview
+            title={title}
+            description={description}
+            slug={slug}
+            imageData={imageData}
+          />
+        );
+      })}
+    </Layout>
+  )
+};
