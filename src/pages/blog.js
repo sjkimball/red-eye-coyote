dd9ed4c6d580899d18d233a66b53c3f161582f4b
Sam Kimball <samuel.j.kimball@gmail.com>
2019-08-18 12:29:11 -0500

    Create blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,16 @@
+import React from "react"
+// import { graphql, useStaticQuery } from 'gatsby';
+
+import Layout from "../components/layout"
+// import Profile from "../components/profile"
+import SEO from "../components/seo"
+
+export default () => {
+
+	return(
+		<Layout>
+			<SEO title="Blog" />
+			<h1>Hello Blog</h1>
+		</Layout>
+	)
+};
