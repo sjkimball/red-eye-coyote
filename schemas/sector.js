d829d39302c035e92c51939210be9a078a52085e
Sam Kimball <samuel.j.kimball@gmail.com>
2019-08-18 23:58:32 -0500

    Create sector schema

---
+++ schemas/sector.js
@@ -0,0 +1,18 @@
+export default {
+  name: 'sector',
+  title: 'Sector',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string'
+    },
+    {
+      name: 'definition',
+      title: 'Definition',
+      type: 'text'
+    }
+  ],
+  hidden: true
+}
