926f8e2af69fee45cbf8ac009e4bc4fbd62b0bf6
Sam Kimball <samuel.j.kimball@gmail.com>
2019-08-19 00:01:53 -0500

    Add references to new schemas, add image fields

--- schemas/project.js
+++ schemas/project.js
@@ -2,11 +2,43 @@ export default {
 	name: 'project',
 	title: 'Project',
 	type: 'document',
+	fieldsets: [
+		{name: 'coverImage', title: 'Cover Image'}
+	],
 	fields: [
 		{
 			name: 'title',
 			title: 'Title',
 			type: 'string'
+		},
+		{
+			name: 'client',
+			title: 'Client',
+			type: 'reference',
+			to: [
+				{type: 'client'}
+			]
+		},
+		{
+			name: 'office',
+			title: 'Office',
+			type: 'reference',
+			to: [
+				{type: 'office'}
+			]
+		},
+		{
+			name: 'creators',
+			title: 'Creators',
+			type: 'array',
+			of: [
+				{
+					type: 'reference',
+					to: [
+						{type: 'person'}
+					]
+				}
+			]
 		},
 		{
 			name: 'slug',
@@ -24,25 +56,53 @@ export default {
 			of: [{type: 'reference', to: {type: 'discipline'}}]
 		},
 		{
+			name: 'sectors',
+			title: 'Sectors',
+			type: 'array',
+			of: [{type: 'reference', to: {type: 'sector'}}]
+		},
+		{
 			name: 'project_summary',
 			title: 'Project Summary',
 			type: 'text'
 		},
 		{
-			name: 'mainImage',
-			title: 'Main Image',
-			type: 'reference',
-			to: [
+			name: 'primaryImg',
+			title: 'Image',
+			type: 'image',
+			fieldset: 'coverImage'
+		},
+		{
+			name: 'primaryImg_alt',
+			title: 'Alternative Text',
+			type: 'string',
+			fieldset: 'coverImage'
+		},
+		{
+			name: 'primaryImg_cap',
+			title: 'Caption',
+			type: 'text',
+			fieldset: 'coverImage'
+		},
+		{
+			name: 'supportingImgs',
+			title: 'Supporting Images',
+			type: 'array',
+			of: [
 				{
 					type: 'customImage'
 				}
-			]
+			],
+			options: {
+				layout: 'grid'
+			}
 		},
 	],
 
 	preview: {
 		select: {
 			title: 'title',
+			subtitle: 'client.client_name'
 		}
 	}
 }
