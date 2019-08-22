de30a50dccdf4df5ab8f286a4c57467a6274cd57
Sam Kimball <samuel.j.kimball@gmail.com>
2019-08-22 01:02:21 -0500

    All client slugs to links

--- src/components/project-preview.js
+++ src/components/project-preview.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Img from 'gatsby-image';
 import { Link } from 'gatsby';
 
-const ProjectPreview = ({ slug, primaryImgData, title, primaryImgAlt, clientName}) => (
+const ProjectPreview = ({ clientName, clientSlug, primaryImgAlt, primaryImgData, title, slug}) => (
 	<div className="project-preview">
-		<Link to={`/${slug}`}>
+		<Link to={`/${clientSlug}/${slug}`}>
 				<Img fluid={primaryImgData} alt={primaryImgAlt} />
 		</Link>
 		<h2>
-			<Link to={`/${slug}/`}>{title}</Link>
+			<Link to={`/${clientSlug}/${slug}/`}>{title}</Link>
 		</h2>
 		<p>{clientName}</p>
 		<p>
