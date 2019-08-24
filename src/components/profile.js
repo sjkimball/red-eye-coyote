1c9f1dba1502a94370a775b878490c5f800ea00b
Sam Kimball <samuel.j.kimball@gmail.com>
2019-08-23 23:38:49 -0500

    Update markdown structure

--- src/components/profile.js
+++ src/components/profile.js
@@ -5,22 +5,29 @@ import BlockContent from '@sanity/block-content-to-react';
 
 const Profile = ({name, bio, imageData }) => {
 
-	const serializers = {
-		types: {
-			children: props => (
-				<pre>
-					{props.node.children}
-				</pre>	
-			)
-		}
-	}
+	// const serializers = {
+	// 	types: {
+	// 		children: props => (
+	// 			<pre>
+	// 				{props.node.children}
+	// 			</pre>	
+	// 		)
+	// 	}
+	// }
 
 	return (
-		<div>
-			<Img fluid={imageData} alt={name} />
-			<h1>Hey. I'm Sam.</h1>
-			<BlockContent blocks={bio} serializers={serializers} />			
-		</div>
+		<article>
+			<header>
+				<figure>
+					<Img fluid={imageData} alt={name} />
+					<figcaption>{name}</figcaption>				
+				</figure>			
+			</header>
+			<section>
+				<h1>Hey. I'm Sam.</h1>
+				<BlockContent blocks={bio} />			
+			</section>
+		</article>
 	);
 };
 
