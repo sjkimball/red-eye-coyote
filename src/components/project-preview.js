f36556a4204d9510922d070c4f95b0675f2cc14d
Sam Kimball <samuel.j.kimball@gmail.com>
2019-08-25 17:04:20 -0500

    Update slug

--- src/components/project-preview.js
+++ src/components/project-preview.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby';
 
 const ProjectPreview = ({ clientName, clientSlug, primaryImgAlt, primaryImgData, title, slug}) => (
 	<div className="project-preview">
-		<Link to={`/${clientSlug}/${slug}`}>
+		<Link to={`/work/${clientSlug}/${slug}`}>
 				<Img fluid={primaryImgData} alt={primaryImgAlt} />
 		</Link>
 		<h2>
