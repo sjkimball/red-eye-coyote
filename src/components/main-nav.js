23959d335efec6513abe9e109b414cfb81e7d359
Sam Kimball <samuel.j.kimball@gmail.com>
2019-08-25 21:22:34 -0500

    Add NavLink components to MainNav

--- src/components/main-nav.js
+++ src/components/main-nav.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Link } from 'gatsby'
+import NavLink from './nav-link'
+
+const navStyles = {
+	backgroundColor: `var(--gray5transparent)`,
+	height: `50vh`,
+	width: `100vw`,
+	position: `fixed`,
+	top: `0`,
+	left: `0`,
+	zIndex: `10`
+}
 
 const MainNav = () => {
 	return (
-		<nav>
-			<ul>
-				<Link to="/work"><li>Work</li></Link>
-				<Link to="/about"><li>About</li></Link>
-				<Link to="/blog"><li>Blog</li></Link>
-				<Link to="/contact"><li>Contact</li></Link>
-			</ul>
+		<nav style={navStyles}>
+			<NavLink to="/work">Work</NavLink>
+			<NavLink to="/about">About</NavLink>
+			<NavLink to="/blog">Blog</NavLink>
+			<NavLink to="/contact">Contact</NavLink>
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
 		</nav>
 	);
 }
