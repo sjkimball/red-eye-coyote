b7379d02675fe7163cf985de8979934df5a546e8
Sam Kimball <samuel.j.kimball@gmail.com>
2019-08-27 17:19:16 -0500

    Update slug in blog post preview

--- src/components/post-preview.js
+++ src/components/post-preview.js
@@ -4,15 +4,15 @@ import { Link } from 'gatsby';
 
 const PostPreview = ({ primaryImgData, title, slug}) => (
 	<div className="post-preview">
-		<Link to={`/blog/`}>
+		<Link to={`/blog/${slug}`}>
 				<Img fluid={primaryImgData} alt={`something`} />
 		</Link>
 		<h2>
-			<Link to={`/${slug}/`}>{title}</Link>
+			<Link to={`/blog/${slug}/`}>{title}</Link>
 		</h2>
 		<p>{title}</p>
 		<p>
-			<Link to={`/${slug}/`}>View this post &rarr;</Link>
+			<Link to={`/blog/${slug}/`}>View this post &rarr;</Link>
 		</p>
 	</div>
 )
