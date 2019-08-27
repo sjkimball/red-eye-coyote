0c4e972e7c970934bdd6bfe383c756a03165d34c
Sam Kimball <samuel.j.kimball@gmail.com>
2019-08-26 21:20:31 -0500

    Add ButtonMenu to header component

--- src/components/header.js
+++ src/components/header.js
@@ -2,40 +2,63 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
-import MenuIcon from './icons/menu-icon'
+import ButtonMenu from './buttons/button-menu'
+import MainNav from './main-nav'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `white`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuVisible: false
+    };
+    this.menuToggle = this.menuToggle.bind(this);
+  }
+
+  menuToggle() {
+    this.setState({
+      menuVisible: !this.state.menuVisible
+    })
+  }
+
+  render() {
+    const { siteTitle } = this.props;
+    const headerColor = this.state.menuVisible ? `red` : `black`;
+    return (
+      <header
+        style={{
+          // background: `white`,
+          marginBottom: `1.45rem`,
+        }}
+      >
+        <div
           style={{
-            color: `black`,
-            fontFamily: `var(--header-font)`,
-            fontWeight: `400`,
-            textDecoration: `none`,
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `1.45rem 1.0875rem`,
+            display: `flex`,
+            justifyContent: `space-between`
           }}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-      <MenuIcon />
-    </div>
-  </header>
-)
-
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: `${headerColor}`,
+                fontFamily: `var(--header-font)`,
+                fontWeight: `400`,
+                textDecoration: `none`,
+              }}
+            >
+              {siteTitle}
+            </Link>
+          </h1>
+          <ButtonMenu onPress={this.menuToggle} menuVisible={this.state.menuVisible}/>
+        </div>
+        {this.state.menuVisible ? <MainNav/> : ``}
+      </header>
+      );
+  }
+}
 Header.propTypes = {
   siteTitle: PropTypes.string,
 }
