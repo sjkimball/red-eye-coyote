9bb62829f917d3331038b5d5ae70320fedfef8eb
Sam Kimball <samuel.j.kimball@gmail.com>
2019-08-26 21:19:36 -0500

    Refactor structure of menu-icon component

--- src/components/icons/menu-icon.js
+++ src/components/icons/menu-icon.js
@@ -1,15 +1,21 @@
 import React from 'react'
 
-const MenuIcon = () => (
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		width="24"
-		height="24"
-		viewBox="0 0 24 24"
-	>
-		<path fill="none" d="M0 0h24v24H0V0z" />
-		<path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
-	</svg>
-);
+const MenuIcon = (props) => {
+		const { menuVisible } = props;
+		const color = menuVisible ? 'white' : '';
+		console.log('color', color);
+		return (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill={color}
+			>
+				<path fill="none" d="M0 0h24v24H0V0z" />
+				<path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+			</svg>
+		);		
+}
 
 export default MenuIcon;
