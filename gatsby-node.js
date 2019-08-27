94c2bd1623348e737f373f1cb8022840f24a6fcc
Sam Kimball <samuel.j.kimball@gmail.com>
2019-08-27 17:20:58 -0500

    Update createPages for blog post pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,8 +6,8 @@
 
 // You can delete this file if you're not using it
 
-exports.createPages = async ({ actions, graphql, reporter }) => {
-	const result = await graphql(`
+exports.createPages = async ({ actions: { createPage }, graphql, reporter }) => {
+	const allProjects = await graphql(`
 		{
 			allSanityProject {
 				edges {
@@ -26,21 +26,54 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 		}
 	`);
 
-	if (result.error) {
+	if (allProjects.error) {
 		reporter.panic('There was a problem loading the project.');
 		return;
 	}
 
-	const projects = result.data.allSanityProject.edges;
+	const allPosts = await graphql(`
+		{
+			allSanityPost {
+			  edges {
+			    node {
+			      slug {
+			        current
+			      }
+			    }
+			  }
+			}
+		}
+	`);
+
+	if (allPosts.error) {
+		reporter.panic('There was a problem loading the post.');
+		return;
+	}
+
+
+	const projects = allProjects.data.allSanityProject.edges;
+	const posts = allPosts.data.allSanityPost.edges;
+
 
 	projects.forEach(({ node: project }) => {
 
-		actions.createPage({
+		createPage({
 			path: `/work/${project.client.slug.current}/${project.slug.current}`,
 			component: require.resolve('./src/templates/project.js'),
 			context: { 
 					slug: project.slug.current,
 					clientSlug: project.client.slug.current
+				}
+		});
+	});
+
+	posts.forEach(({ node: post }) => {
+
+		createPage({
+			path: `/blog/${post.slug.current}`,
+			component: require.resolve('./src/templates/post.js'),
+			context: { 
+					slug: post.slug.current
 				}
 		});
 	});
