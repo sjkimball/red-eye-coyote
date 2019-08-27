91ce285e18f9a57a8c3803f8fc0953057a256ca2
Sam Kimball <samuel.j.kimball@gmail.com>
2019-08-27 00:31:14 -0500

    Add styling to nav links

--- src/components/nav-link.js
+++ src/components/nav-link.js
@@ -2,19 +2,20 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const linkStyles = {
-	display: `block`,
 	color: `white`,
-	textDecoration: `none`,
-	letterSpacing: `0.5px`,
-	textTransform: `uppercase`,
-	fontWeight: `500`,
+	display: `block`,
 	fontSize: `17px`,
-	marginBottom: `17px`
+	fontWeight: `500`,
+	letterSpacing: `0.5px`,
+	marginBottom: `8.5px`,
+	padding: `8.5px`,
+	textDecoration: `none`,
+	textTransform: `uppercase`
 }
 
 const activeStyle = {
-	color: `white`,
-	backgroundColor: `red`,
+	border: `1px solid var(--accent1)`,
+	color: `white`
 }
 
 const NavLink = ({children, to}) => (
