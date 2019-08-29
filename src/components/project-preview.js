62b48ce24f36219170e5b82f4cbb2b079eed9447
Sam Kimball <samuel.j.kimball@gmail.com>
2019-08-28 19:57:19 -0500

    Add styling to project preview

--- src/components/project-preview.js
+++ src/components/project-preview.js
@@ -2,18 +2,35 @@ import React from 'react';
 import Img from 'gatsby-image';
 import { Link } from 'gatsby';
 
+const containerStyles = {
+	marginBottom: `34px`
+}
+
+const previewStyles = {
+	textDecoration: `none`,	
+}
+
+const imgStyles = {
+	marginBottom: `8.5px`
+}
+
+const titleStyles = {
+	fontSize: `17px`,
+	fontWeight: `500`,
+	color: `var(--gray5)`
+}
+
 const ProjectPreview = ({ clientName, clientSlug, primaryImgAlt, primaryImgData, title, slug}) => (
-	<div className="project-preview">
-		<Link to={`/work/${clientSlug}/${slug}`}>
-				<Img fluid={primaryImgData} alt={primaryImgAlt} />
+	<div className="project-preview" style={containerStyles}>
+		<Link to={`/work/${clientSlug}/${slug}`} style={previewStyles}>
+			<Img
+				fluid={primaryImgData}
+				alt={primaryImgAlt}
+				sizes={{ ...primaryImgData, aspectRatio: 16 / 9 }}
+				style={imgStyles}
+			/>
+			<h2 style={titleStyles}>{title}</h2>
 		</Link>
-		<h2>
-			<Link to={`/${clientSlug}/${slug}/`}>{title}</Link>
-		</h2>
-		<p>{clientName}</p>
-		<p>
-			<Link to={`/${slug}/`}>View this project &rarr;</Link>
-		</p>
 	</div>
 )
 
