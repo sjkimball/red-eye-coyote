14845fa5af5d15321628b81af3997330c7d53a3c
Sam Kimball <samuel.j.kimball@gmail.com>
2019-08-30 01:17:42 -0500

    Move preview styles to external sheet, update classNames

--- src/components/post-preview.js
+++ src/components/post-preview.js
@@ -2,18 +2,19 @@ import React from 'react';
 import Img from 'gatsby-image';
 import { Link } from 'gatsby';
 
+import './preview.css';
+
 const PostPreview = ({ primaryImgData, title, slug}) => (
-	<div className="post-preview">
-		<Link to={`/blog/${slug}`}>
-				<Img fluid={primaryImgData} alt={`something`} sizes={{...primaryImgData, aspectRatio: 16 / 9 }}/>
+	<div className={`rec-container`}>
+		<Link to={`/blog/${slug}`} className={`post-preview`}>
+				<Img
+					fluid={primaryImgData}
+					alt={`something`}
+					sizes={{...primaryImgData, aspectRatio: 16 / 9 }}
+					className={`post-thumb`}
+				/>
+				<h2 className={`post-title`}>{title}</h2>
 		</Link>
-		<h2>
-			<Link to={`/blog/${slug}/`}>{title}</Link>
-		</h2>
-		<p>{title}</p>
-		<p>
-			<Link to={`/blog/${slug}/`}>View this post &rarr;</Link>
-		</p>
 	</div>
 )
 
--- src/components/project-preview.js
+++ src/components/project-preview.js
@@ -2,34 +2,18 @@ import React from 'react';
 import Img from 'gatsby-image';
 import { Link } from 'gatsby';
 
-const containerStyles = {
-	marginBottom: `34px`
-}
-
-const previewStyles = {
-	textDecoration: `none`,	
-}
-
-const imgStyles = {
-	marginBottom: `8.5px`
-}
-
-const titleStyles = {
-	fontSize: `17px`,
-	fontWeight: `500`,
-	color: `var(--gray5)`
-}
+import './preview.css';
 
 const ProjectPreview = ({ clientName, clientSlug, primaryImgAlt, primaryImgData, title, slug}) => (
-	<div className="project-preview" style={containerStyles}>
-		<Link to={`/work/${clientSlug}/${slug}`} style={previewStyles}>
+	<div className={`rec-container`}>
+		<Link to={`/work/${clientSlug}/${slug}`} className={`project-preview`}>
 			<Img
 				fluid={primaryImgData}
 				alt={primaryImgAlt}
 				sizes={{ ...primaryImgData, aspectRatio: 16 / 9 }}
-				style={imgStyles}
+				className={`project-thumb`}
 			/>
-			<h2 style={titleStyles}>{title}</h2>
+			<h2 className={`project-title`}>{title}</h2>
 		</Link>
 	</div>
 )
