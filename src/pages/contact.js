40c4ad803558219720229a08d425c93239f7d493
Sam Kimball <samuel.j.kimball@gmail.com>
2019-08-30 09:47:12 -0500

    add heading

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,14 +3,14 @@ import React from "react"
 import Layout from "../components/layout"
 // import Profile from "../components/profile"
 import SEO from "../components/seo"
-import ContactForm from '../components/contact-form'
+// import ContactForm from '../components/contact-form'
 
 export default () => {
 
 	return(
 		<Layout>
 			<SEO title="Contact" />
-			<ContactForm />
+			<h1>Contact</h1>
 		</Layout>
 	)
 };
