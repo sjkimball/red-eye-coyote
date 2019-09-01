9598b3e6ff8ba8804d43c650d0159f0e18dd2c0f
Sam Kimball <samuel.j.kimball@gmail.com>
2019-08-31 20:49:54 -0500

    Add conditional rendering based on header class

--- src/components/icons/menu-icon.js
+++ src/components/icons/menu-icon.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
 const MenuIcon = (props) => {
+	const { headerClass } = props;
+	let color = '';
+	if (headerClass === `dark`) {
+		color = `white`;
+	} else if (headerClass === `light`) {
+		color = `hsl(0, 0%, 5%)`
+	}
 		return (
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				width="24"
 				height="24"
 				viewBox="0 0 24 24"
-				fill={props.headerColor}
+				fill={color}
 			>
 				<path fill="none" d="M0 0h24v24H0V0z" />
 				<path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
