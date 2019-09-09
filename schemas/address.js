db066427f047edee38474fa41a5b37151b2ebcde
Sam Kimball <samuel.j.kimball@gmail.com>
2019-09-08 19:05:31 -0500

    Create address schema

---
+++ schemas/address.js
@@ -0,0 +1,42 @@
+export default {
+	name: 'address',
+	title: 'Address',
+	type: 'object',
+	fields: [
+		{
+			name: 'streetNO',
+			title: 'Street Number',
+			type: 'string'
+		},
+		{
+			name: 'street',
+			title: 'Street',
+			type: 'string'
+		},
+		{
+			name: 'aptSuiteUnit',
+			title: 'Apartment or Suite',
+			type: 'string'
+		},
+		{
+			name: 'city',
+			title: 'City',
+			type: 'string',
+		},
+		{
+			name: 'state_province',
+			title: 'State/Province',
+			type: 'string',
+		},
+		{
+			name: 'country',
+			title: 'Country',
+			type: 'string',
+		},
+		{
+			name: 'postalCode',
+			title: 'Postal Code',
+			type: 'string'
+		}
+	]
+}
