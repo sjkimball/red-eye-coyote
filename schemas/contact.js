d039f3840fa530b441888359ef96ea2c2d3240a6
Sam Kimball <samuel.j.kimball@gmail.com>
2019-09-08 19:06:04 -0500

    Create contact schema

---
+++ schemas/contact.js
@@ -0,0 +1,17 @@
+export default {
+	name: 'contact',
+	title: 'Contact Information',
+	type: 'object',
+	fields: [
+		{
+			name: 'email',
+			title: 'Email Address',
+			type: 'string'
+		},
+		{
+			name: 'phone',
+			title: 'Phone Number',
+			type: 'string'
+		}
+	]
+}
