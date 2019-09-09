fd1294c4b1786d15eab3597386a4d12ca4d21494
Sam Kimball <samuel.j.kimball@gmail.com>
2019-09-08 19:23:11 -0500

    Create footer component

---
+++ src/components/footer.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import { useStaticQuery, graphql } from 'gatsby'
+
+import './footer.css'
+
+const Footer = (props) => {
+	const data = useStaticQuery(graphql`
+		query primaryOfficeInfo {
+		  sanityOffice(address: {city: {eq: "Chicago"}}){
+		    address {
+		      city
+		      state_province
+		      country
+		    }
+		    links {
+		    	_key
+		      service {
+		        service
+		      }
+		      username
+		      url
+		    }
+		  }
+		}
+	`)
+
+	const office = data.sanityOffice;
+	const socialList = office.links;
+
+	return (
+		<footer className={`footer`}>
+			<ul className={`social-list`}>
+				{socialList.map((link) => {
+					return (
+						<a key={link._key} href={link.url} target="_blank" rel="noopener noreferrer">
+							<li>{link.service.service}</li>
+						</a>
+					);
+				})}
+			</ul>
+			<small className={`copyright`}>© Sam Kimball</small>	
+		</footer>
+	)
+}
+
+export default Footer;
