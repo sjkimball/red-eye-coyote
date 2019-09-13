d7721a1b4b63af2f5dc88510e4ca4bcfe9409776
Sam Kimball <samuel.j.kimball@gmail.com>
2019-09-13 14:05:15 -0500

    Update props, heading structure, media query

--- src/components/office-contact.js
+++ src/components/office-contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
+
 import Img from 'gatsby-image';
-// import BlockContent from '@sanity/block-content-to-react';
 
 import './office-contact.css'
 
@@ -9,21 +9,21 @@ const OfficeContact = ({office}) => {
 		<article className={`rec-office`}>
 			<header>
 				<Img
-					fluid={office.image.img_file.asset.fluid}
+					fluid={office.image.file.asset.fluid}
 					alt={office.image.alt_text}
-					sizes={{...office.image.img_file.asset.fluid, aspectRatio: 4 / 3 }}
-					style={{marginBottom: '1rem'}}
+					sizes={{...office.image.file.asset.fluid, aspectRatio: 4 / 3 }}
+					className={`header-image`}
 				/>				
 			</header>
 			<section>
-				<h6>Email</h6>
+				<h2>Email</h2>
 				<a href={`mailto:${office.contact.email}`}>
 					<span>
 						{office.contact.email}								
 					</span>
 				</a>
 
-				<h6>Phone</h6>
+				<h2>Phone</h2>
 				<a href={`tel:1-312-879-9813`}>
 					<span>
 						{office.contact.phone}
