3203715b9a552ec53af62e09a8eb0697d9ce4b2d
Sam Kimball <samuel.j.kimball@gmail.com>
2019-09-13 14:09:30 -0500

    Add primaryImage type

--- schemas/blockContent.js
+++ schemas/blockContent.js
@@ -55,8 +55,7 @@ export default {
     // primitive types such as 'string' and 'number' in the same array
     // as a block type.
     {
-      type: 'image',
-      options: {hotspot: true}
+      type: 'primaryImage'
     }
   ]
 }
