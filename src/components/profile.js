11ef2c399a23fc437609e2b11ea1ab6d1267e0e9
Sam Kimball <samuel.j.kimball@gmail.com>
2020-02-23 13:31:19 -0600

    Implement PreviewImage component, adjust spacing

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,22 +1,12 @@
 import React from 'react';
 
-import useSanityOptions from "../hooks/use-sanity-options";
-
-import imageUrlBuilder from '@sanity/image-url'
-
 import BlockContent from '@sanity/block-content-to-react';
+
+import PreviewImage from '../components/preview-image';
 
 import './profile.css';
 
 const Profile = ({bio, imageData, name, office, personDoc, socialAccounts }) => {
-
-	const mySanityConfig = useSanityOptions();
-
-	const builder = imageUrlBuilder(mySanityConfig);
-
-	function urlFor(source) {
-		return builder.image(source)
-	}
 
 	const serializers = {
 		types: {
@@ -27,22 +17,18 @@ const Profile = ({bio, imageData, name, office, personDoc, socialAccounts }) => 
 
 	return (
 		<article className={`profile`}>
-			<header className={`profile-header`}>
+			<header className={`page-header profile-header`}>
 				<h1>{name}
 					<br/>
 					<span>{office}</span>
 				</h1>
-				<img
-					src={urlFor(imageData.image).width(500).url()}
-					alt={imageData.alt_text}
-					className={`profile-img`}
-				/>
+				<PreviewImage imageAsset={imageData} />
 			</header>
 			<BlockContent blocks={bio} serializers={serializers} className={`profile-bio`}/>				
 			<section className={`profile-info`}>
 				<h6>Additional Information</h6>
-				<a href={`https://indd.adobe.com/view/3cf8b7d4-92ea-4eb6-af0d-a0677a8e392d`} target="_blank" rel="noopener noreferrer"><span>Resume</span></a>
-				<a href={`https://www.linkedin.com/in/sjkimball/`} target="_blank" rel="noopener noreferrer"><span>LinkedIn</span></a>
+				<a href={`https://indd.adobe.com/view/3cf8b7d4-92ea-4eb6-af0d-a0677a8e392d`} target="_blank" rel="noopener noreferrer">Resume</a>
+				<a href={`https://www.linkedin.com/in/sjkimball/`} target="_blank" rel="noopener noreferrer">LinkedIn</a>
 			</section>			
 		</article>
 	);
