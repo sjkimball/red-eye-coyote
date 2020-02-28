5ef85551032bc626df3f8103955bc2b24f074c1b
Sam Kimball <samuel.j.kimball@gmail.com>
2020-02-28 16:43:05 -0600

    Update office query and OfficeContact component

--- src/components/office-contact.js
+++ src/components/office-contact.js
@@ -1,35 +1,42 @@
 import React from 'react';
 
-// import PreviewImage from '../components/preview-image';
+import PreviewImage from '../components/preview-image';
 
 import './office-contact.css'
 
 const OfficeContact = ({office}) => {
+	const { images, contact_info, description } = office;
+	const address = contact_info.address;
+
 	return (
 		<article className={`rec-office`}>
 			<header>
-				{/*<PreviewImage imageAsset={office.office_image} />*/}
-				<h2>Lake Geneva</h2>
+				{images.map((image) => {
+					return (
+						<PreviewImage key={image.image.asset._id} imageAsset={image} />
+					)
+				})}
+				<h2>{address.city}</h2>
 				<time>Current time</time>
 			</header>
 			<section className={`office-info`}>
 				<section className={`office-contact`}>
-					<a href={`mailto:${office.contact_info.email}`} className={`office-email`}>
-						{office.contact_info.email}
+					<a href={`mailto:${contact_info.email}`} className={`office-email`}>
+						{contact_info.email}
 					</a>
 
 					<a href={`tel:1-312-879-9813`} className={`office-phone`}>
-						+1 {office.contact_info.phone}
-					</a>					
+						+1 {contact_info.phone}
+					</a>
 				</section>
 				<address>
-					651 Southwind Drive<br/>
-					Unit 103<br/>
-					Lake Geneva, WI 53147<br/>
-					United States
+					{address.street_number} {address.street}<br/>
+					{address.street_secondary}<br/>
+					{address.city}, {address.state} {address.postalCode}<br/>
+					{address.country}
 				</address>
 				<p>
-					Description of location
+					{description}
 				</p>
 			</section>
 		</article>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,45 +7,64 @@ import Layout from '../components/layout'
 import OfficeContact from '../components/office-contact'
 
 const ContactPage = ({data}) => {
-
-	const office = data.sanityOffice;
-
+	const offices = data.allSanityOffice.edges;
 	return(
 		<Layout>
 			<SEO title="Contact" />
 			<header className={`page-header page-header--light`}>
 				<h1>Contact</h1>
 			</header>
-			<OfficeContact office={office} />
+			{offices.map(({node: office}) => {
+				return (
+					<OfficeContact key={office._id} office={office} />
+				);
+			})}
 		</Layout>
 	)
 }
 
 export const query = graphql`
-	query primaryContact {
-	  sanityOffice(contact_info: {address: {city: {eq: "Chicago"}}}) {
-	    images {
-	      image {
-	        asset {
-	          _id
+	query OfficeContact {
+	  allSanityOffice(sort: {fields: contact_info___address___city}) {
+	    edges {
+	      node {
+	      	_id
+	        images {
+	          alt_text
+	          caption
+	          image {
+	            asset {
+	              _id
+	            }
+	            crop {
+	              bottom
+	              left
+	              right
+	              top
+	            }
+	            hotspot {
+	              height
+	              width
+	              x
+	              y
+	            }
+	          }
 	        }
-	        crop {
-	        	top
-	        	bottom
-	        	left
-	        	right
+	        contact_info {
+	          address {
+	            city
+		          street_number
+		          street
+		          street_secondary
+		          state_province
+		          postalCode
+		          country
+	          }
+	          email
+	          phone
 	        }
-	        hotspot {
-	        	x
-	        	y
-	        	height
-	        	width
-	        }
+	        description
 	      }
-	    }
-	    contact_info {
-	      email
-	      phone
 	    }
 	  }
 	}
