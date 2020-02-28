20ac1b7ae9a86ad9f2c0d9872c943a190ccdb84e
Sam Kimball <samuel.j.kimball@gmail.com>
2020-02-28 14:10:44 -0600

    Implement useStaffCount hook
    This was done so the user would skip past the generic About page if there was only one staff member and go directly to their individual profile

--- src/components/main-nav.js
+++ src/components/main-nav.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import useStaffCount from '../hooks/use-staff-count';
+
 import NavLink from './nav-link'
 
 const navStyles = {
@@ -15,10 +18,19 @@ const navStyles = {
 }
 
 const MainNav = () => {
+	const data = useStaffCount();
+
+	let staffCount = data.totalCount;
+	let staffMemberSlug = data.edges;
+
 	return (
 		<nav style={navStyles}>
 			<NavLink to="/work">Work</NavLink>
-			<NavLink to="/about">About</NavLink>
+			{(staffCount == 1) ? (
+				<NavLink to={`/about/${staffMemberSlug[0].node.slug.current}`}>About</NavLink>
+			) : (
+				<NavLink to="/about">About</NavLink>
+			)}
 			<NavLink to="/blog">Blog</NavLink>
 			<NavLink to="/contact">Contact</NavLink>
 			<footer style={{ paddingLeft: `8.5px`}}>
