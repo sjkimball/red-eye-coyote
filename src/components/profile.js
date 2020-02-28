42c7c85de2fc1843ebc7c982b7b20d8d11b7eb68
Sam Kimball <samuel.j.kimball@gmail.com>
2020-02-28 14:05:41 -0600

    Update prop names on Profile component

--- src/components/profile.js
+++ src/components/profile.js
@@ -6,11 +6,11 @@ import PreviewImage from '../components/preview-image';
 
 import './profile.css';
 
-const Profile = ({bio, imageData, first_name, last_name, office, personDoc, socialAccounts }) => {
+const Profile = ({bio, profileImg, profileName, office }) => {
 
 	const serializers = {
 		types: {
-			block: props => 
+			block: props =>
 				<p>{props.node.children[0].text}</p>
 		}
 	}
@@ -18,18 +18,18 @@ const Profile = ({bio, imageData, first_name, last_name, office, personDoc, soci
 	return (
 		<article className={`profile`}>
 			<header className={`page-header profile-header`}>
-				<h1>{first_name} {last_name}
+				<h1>{profileName}
 					<br/>
 					<span>{office}</span>
 				</h1>
-				<PreviewImage imageAsset={imageData} />
+				<PreviewImage imageAsset={profileImg} />
 			</header>
-			<BlockContent blocks={bio} serializers={serializers} className={`profile-bio`}/>				
+			<BlockContent blocks={bio} serializers={serializers} className={`profile-bio`}/>
 			<section className={`profile-info`}>
 				<h6>Additional Information</h6>
 				<a href={`https://indd.adobe.com/view/3cf8b7d4-92ea-4eb6-af0d-a0677a8e392d`} target="_blank" rel="noopener noreferrer">Resume</a>
 				<a href={`https://www.linkedin.com/in/sjkimball/`} target="_blank" rel="noopener noreferrer">LinkedIn</a>
-			</section>			
+			</section>
 		</article>
 	);
 };
