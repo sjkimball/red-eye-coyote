d0cd7b1975cd0187b81ff2e2c249947c1bca65a3
Sam Kimball <samuel.j.kimball@gmail.com>
2020-03-05 17:02:12 -0600

    PostPreview stying updates

--- src/components/post-preview.js
+++ src/components/post-preview.js
@@ -10,12 +10,10 @@ const PostPreview = ({ post }) => {
 	const { coverImg, publishedAt, title, slug, subtitle } = post;
 	return(
 		<Link to={`/blog/${slug.current}`} className={`post-preview`}>
-				{<PreviewImage imageAsset={coverImg} />}
-				<div className="post-preview__info">
-					<h2 className={`post-preview__title`}>{title}</h2>
-					<time className={`post-preview__time`} dateTime={publishedAt}>{publishedAt}</time>
-					<h4 className={`post-preview__subtitle`}>{subtitle}</h4>
-				</div>
+			<h2 className={`post-preview__title`}>{title}</h2>
+			<time className={`post-preview__time`} dateTime={publishedAt}>{publishedAt}</time>
+			<h4 className={`post-preview__subtitle`}>{subtitle}</h4>
+			{<PreviewImage imageAsset={coverImg} />}
 		</Link>
 	);
 }
