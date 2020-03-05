46ee9b23dab8a9d958acc4efa20483a0d4c76419
Sam Kimball <samuel.j.kimball@gmail.com>
2020-03-05 17:00:48 -0600

    OfficeContact class renaming and styling
    Update with BEM naming convention

--- src/components/office-contact.js
+++ src/components/office-contact.js
@@ -10,32 +10,34 @@ const OfficeContact = ({office}) => {
 
 	return (
 		<article className={`rec-office`}>
-			<header>
-				{images.map((image) => {
-					return (
-						<PreviewImage key={image.image.asset._id} imageAsset={image} />
-					)
-				})}
-				<h2>{address.city}</h2>
-				<time>Current time</time>
+			<header className={`rec-office__header`}>
+				<h2 className={`rec-office__city`}>{address.city}</h2>
+				<time className={`rec-office__time`}>Current time</time>
+				<section className={`rec-office__images`}>
+					{images.map((image) => {
+						return (
+							<PreviewImage key={image.image.asset._id} imageAsset={image} />
+						)
+					})}
+				</section>
 			</header>
-			<section className={`office-info`}>
-				<section className={`office-contact`}>
-					<a href={`mailto:${contactInfo.email}`} className={`office-email`}>
+			<section className={`rec-office__info`}>
+				<section className={`rec-office__contact`}>
+					<a href={`mailto:${contactInfo.email}`} className={`rec-office__email`}>
 						{contactInfo.email}
 					</a>
 
-					<a href={`tel:1-312-879-9813`} className={`office-phone`}>
+					<a href={`tel:1-312-879-9813`} className={`rec-office__phone`}>
 						+1 {contactInfo.phone}
 					</a>
 				</section>
-				<address>
-					{address.street_number} {address.street}<br/>
-					{address.street_secondary}<br/>
+				<address className={`rec-office__address`}>
+					{address.streetNumber} {address.street}<br/>
+					{address.streetSecondary}<br/>
 					{address.city}, {address.state} {address.postalCode}<br/>
 					{address.country}
 				</address>
-				<p>
+				<p className={`rec-office__description`}>
 					{description}
 				</p>
 			</section>
