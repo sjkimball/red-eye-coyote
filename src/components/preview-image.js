0c8f3cd322b4f368a999c2d99b897986edebab91
Sam Kimball <samuel.j.kimball@gmail.com>
2020-03-19 17:04:22 -0500

    Destructure imageAsset, hash out PreviewImage sizes

--- src/components/preview-image.js
+++ src/components/preview-image.js
@@ -5,7 +5,6 @@ import imageUrlBuilder from '@sanity/image-url'
 import useSanityOptions from "../hooks/use-sanity-options";
 
 const PreviewImage = ({ imageAsset, showCaption }) => {
-
 	const mySanityConfig = useSanityOptions();
 
 	const builder = imageUrlBuilder(mySanityConfig);
@@ -14,16 +13,52 @@ const PreviewImage = ({ imageAsset, showCaption }) => {
 		return builder.image(source)
 	}
 
+	const {
+		altText,
+		caption,
+		image: {
+			asset: {
+				_id,
+				metadata
+			}
+		}
+	} = imageAsset;
+
+	// Need to implement lqip as backgroundImage
+	// console.log(metadata.lqip);
+
+	const image = (
+		<img
+			srcSet={`
+        ${urlFor(_id).width(400)} 400w,
+        ${urlFor(_id).width(600)} 600w,
+        ${urlFor(_id).width(400).dpr(2)} 800w,
+        ${urlFor(_id).width(600).dpr(2)} 1200w
+      `}
+      sizes={`
+        (max-width: 320px) 288px,
+        (max-width: 480px) 448px,
+        (max-width: 759px) 700px,
+        475px
+      `}
+			src={urlFor(_id)
+				.auto('format')
+				}
+			alt={altText}
+			className={"preview-image"}
+		/>
+	);
+
 	if (showCaption === true) {
 		return (
 			<figure className={`preview-image`}>
-				<img src={urlFor(imageAsset.image).auto('format').url()} alt={imageAsset.altText} className={`preview-image`}/>
-				<figcaption>{imageAsset.caption}</figcaption>
+				{image}
+				<figcaption>{caption}</figcaption>
 			</figure>
 			);
 	} else {
 			return (
-				<img src={urlFor(imageAsset.image).auto('format').url()} alt={imageAsset.altText} className={`preview-image`}/>
+				image
 			);
 	}
 }
