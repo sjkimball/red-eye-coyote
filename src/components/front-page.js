55a98808dfa70b1c957a76fe2ee4720565ca16fb
Sam Kimball <samuel.j.kimball@gmail.com>
2020-03-26 13:32:16 -0500

    Update FrontPage component structure

--- src/components/front-page.js
+++ src/components/front-page.js
@@ -2,41 +2,39 @@ import React from 'react'
 
 import { Link } from 'gatsby';
 
+import CoverImage from '../components/cover-image';
 import PreviewImage from '../components/preview-image';
 
 import './front-page.css'
 
 const FrontPage = ({ pageContent }) => {
-	// console.log(pageContent);
-	// const { heading, subheading, heroImage, workSamples } = pageContent;
 	const { heroImage, workSamples } = pageContent;
 
 	return (
 		<React.Fragment>
-			{/*<h1>{heading}</h1>
-			<h2>{subheading}</h2>*/}
-			<figure className={`hero-image`}>
-				<PreviewImage imageAsset={heroImage} />
-				<figcaption>{heroImage.caption}</figcaption>
-			</figure>
-			<section className="work-samples preview-container">
-				{workSamples.map((sample) => {
-					const key = sample._id;
-					const title = sample.title;
-					const coverImg = sample.coverImg;
-					const clientSlug = sample.client.slug.current;
-					const slug = sample.slug.current;
+			<header id={`landingHeader`} className={`landing-header`}>
+				<CoverImage id={`hero`} imageAsset={heroImage} showCaption={true}/>
+			</header>
+			<article>
+				<section className="work-samples preview-container">
+					{workSamples.map((sample) => {
+						const key = sample._id;
+						const title = sample.title;
+						const coverImg = sample.coverImg;
+						const clientSlug = sample.client.slug.current;
+						const slug = sample.slug.current;
 
-					return (
-						<Link key={key}to={`/work/${clientSlug}/${slug}`}>
-							<PreviewImage imageAsset={coverImg} />
-							<h5>
-								{title}
-							</h5>
-						</Link>
-					);
-				})}
-			</section>
+						return (
+							<Link key={key}to={`/work/${clientSlug}/${slug}`}>
+								<PreviewImage imageAsset={coverImg} />
+								<h5>
+									{title}
+								</h5>
+							</Link>
+						);
+					})}
+				</section>
+			</article>
 		</React.Fragment>
 	)
 }
