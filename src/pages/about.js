edb5e28b1fa1b91f272ceec08cedd6a63d86afcc
Sam Kimball <samuel.j.kimball@gmail.com>
2020-03-30 12:27:48 -0500

    Update About page class names

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,31 +12,31 @@ const AboutPage = ({data}) => {
 	const profiles = profileData.edges;
 
 	return (
-		<Layout page={`about`}>
+		<Layout>
 			<SEO title="About" />
-			<article className={`about-grid`}>
+			<article className={`about`}>
 				<header className={`page-header page-header--dark about__header`}>
-				  <h1>About Red Eye Coyote</h1>
-				  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam nulla animi, consectetur cumque quas neque iste. Delectus tempora voluptatem quasi quae, eum sapiente perspiciatis, dolorem inventore et amet laboriosam eveniet!
-				  </p>
-				  <p>Voluptatibus voluptate ad, sit enim quisquam! Odit blanditiis praesentium animi, voluptatum aspernatur repellendus? Vitae animi autem cupiditate mollitia esse praesentium, corporis magni, maiores earum modi iusto excepturi labore! Reprehenderit, aut!
-				  </p>
-				  <p>Omnis consequatur autem nostrum voluptatibus earum aspernatur facere laborum ullam iure alias adipisci eligendi saepe est esse assumenda, et veritatis vero molestias magni ad explicabo mollitia pariatur dolor a quam.
-				  </p>
+					<h1>About Red Eye Coyote</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam nulla animi, consectetur cumque quas neque iste. Delectus tempora voluptatem quasi quae, eum sapiente perspiciatis, dolorem inventore et amet laboriosam eveniet!
+					</p>
+					<p>Voluptatibus voluptate ad, sit enim quisquam! Odit blanditiis praesentium animi, voluptatum aspernatur repellendus? Vitae animi autem cupiditate mollitia esse praesentium, corporis magni, maiores earum modi iusto excepturi labore! Reprehenderit, aut!
+					</p>
+					<p>Omnis consequatur autem nostrum voluptatibus earum aspernatur facere laborum ullam iure alias adipisci eligendi saepe est esse assumenda, et veritatis vero molestias magni ad explicabo mollitia pariatur dolor a quam.
+					</p>
 				</header>
 				<section className={`about__gallery`}>
 					{profiles.map(({ node: profile }) => {
-					  return (
+						return (
 							<ProfilePreview
-								key={profile._id}
-								profile={profile}
+							key={profile._id}
+							profile={profile}
 							/>
-						);
+							);
 					})}
 				</section>
 			</article>
 		</Layout>
-	)
+		)
 }
 
 export default AboutPage;
