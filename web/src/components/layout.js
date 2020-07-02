483e5541888bd129c4b55cdfb5e998939e5b6259
Sam Kimball <samuel.j.kimball@gmail.com>
2020-07-02 12:06:23 -0500

    Add hooks to access window and set darkMode

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
 import useCompanyInfo from '../hooks/use-company-info'
@@ -10,15 +10,17 @@ import './layout.css'
 
 const Layout = ({children}) => {
   const { companyInfo, staffCount } = useCompanyInfo()
+  const [darkMode, setDarkMode] = useState(false)
+  useEffect(() => {
+    const location = window.location.pathname
+    if (location.includes('/about')) {
+      document.body.style.backgroundColor = 'var(--gray5)'
+      setDarkMode(true)
+    } else {
+      document.body.style.backgroundColor = 'white'
+    }    
+  });
 
-  let location = window.location.pathname
-  let darkMode = false
-  if (location.includes('/about')) {
-    document.body.style.backgroundColor = 'var(--gray5)'
-    darkMode = true
-  } else {
-    document.body.style.backgroundColor = 'white'
-  }
   return (
     <React.Fragment>
       <Header siteTitle={companyInfo.companyName} darkMode={darkMode} staffInfo={staffCount} />
