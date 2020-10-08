06ed2318bff5d4287d5234f61571c65c513d903a
Sam Kimball <samuel.j.kimball@gmail.com>
2020-10-08 18:34:21 -0500

    Remove landing page component

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -2,19 +2,32 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import LandingPage from '../components/landing-page'
+import HeroSvg from '../components/astronaut'
 
 import useLandingPage from '../hooks/use-landing-page'
 
 import './pages.css'
 
-const IndexPage = () => {
-  const pageContent = useLandingPage();
+const isDisabled = (item) => {
+  return !item.disabled;
+}
+const isFigure = (item) => {
+  return item._type == 'figure';
+}
 
+const IndexPage = () => {
+  const {heading, subheading, content} = useLandingPage();
+  const figures = content.filter(isDisabled).filter(isFigure)
   return (
     <Layout>
       <SEO title='Home' />
-      <LandingPage pageContent={pageContent} />
+      <article className={`rec-article rec-landing`}>
+        <div className={'rec-landing__headline'}>
+          <h2>{heading}</h2>
+          <h3>{subheading}</h3>
+        </div>
+        <HeroSvg className={`rec-hero`} imageAsset={figures[0]} imageType={`hero`} />          
+      </article>
     </Layout>
   )
 }
