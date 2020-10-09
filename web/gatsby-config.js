d8dc14bcb18e43f7096aeff7ae8c42bf933697b8
Sam Kimball <samuel.j.kimball@gmail.com>
2020-10-08 23:55:49 -0500

    Update manifest

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `browser`,
+        display: `standalone`,
         icon: `src/images/red_eye_coyote_icon.png` // This path is relative to the root of the site.
       }
     },
