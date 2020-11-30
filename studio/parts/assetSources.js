3b9de3dd882acf2ec519e6bb0be07bad9ca0a216
Sam Kimball <samuel.j.kimball@gmail.com>
2020-11-29 22:59:05 -0600

    Update asset sources

---
+++ studio/parts/assetSources.js
@@ -0,0 +1,4 @@
+import MediaAssetSource from 'part:sanity-plugin-media/asset-source'
+import UnsplashImageAssetSource from 'part:sanity-plugin-asset-source-unsplash/image-asset-source'
+
+export default [MediaAssetSource, UnsplashImageAssetSource]
